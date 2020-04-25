0192714f964e4628fd3598614b9fc2d625681e87
MariamElbarbary <mariam.elbarbary@ev-box.com>
2020-04-25 11:34:54 +0200

    add delete functionality to the chore list

--- javascript/current-chores.js
+++ javascript/current-chores.js
@@ -7,6 +7,13 @@ button.addEventListener("click", appendToTheList);
 
 inputText.addEventListener("keypress", appendToListWithKeyPress);
 
+Array.from(choresList.children, addActionsToArray);
+
+function addActionsToArray(item) {
+    item.addEventListener("click", toggleDone);
+    addDeleteButton(item);
+};
+
 
 function inputLength() {
     return inputText.value.length;
@@ -22,7 +29,31 @@ function appendToTheList() {
     if (inputLength() > 0) {
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(inputText.value));
+        li.addEventListener("click", toggleDone);
+
         choresList.appendChild(li);
         inputText.value = "";
+
+        addDeleteButton(li);
     }
+}
+
+function toggleDone(event) {
+    if (!event.srcElement.classList.contains("delete")) {
+        event.srcElement.classList.toggle("done");
+    }
+}
+
+function addDeleteButton(item) {
+    var deletebutton = document.createElement("button");
+    deletebutton.appendChild(document.createTextNode("delete me!"));
+    deletebutton.classList.add("delete");
+    deletebutton.addEventListener("click", deleteItem);
+    item.appendChild(deletebutton);
+}
+
+
+function deleteItem(event) {
+    console.log("delete me!");
+    choresList.removeChild(event.srcElement.parentNode);
 }
